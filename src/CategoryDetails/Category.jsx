77e16fd38244ee330b8c55ec3ddb68cd08033166
afsana-jannat <afsanajannat205@gmail.com>
2023-11-08 02:23:21 +0600

    job cat tab done

--- src/CategoryDetails/Category.jsx
+++ src/CategoryDetails/Category.jsx
@@ -1,28 +1,31 @@
+import { Link } from "react-router-dom";
 
 
 
 const Category = ({ category }) => {
-    
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             {
                 category?.map(item => <div key={item._id}>
 
                     <div className="card bg-base-100 shadow-xl">
-                        <figure><img src={item?.image} alt="Shoes" /></figure>
+                        <figure><img src={item?.image} alt="job" className="h-[280px]"/></figure>
                         <div className="card-body">
                             <div className="flex justify-between">
-                            <h2 className="card-title">{item?.name}</h2>
-                            <h1 className="card-title font-bold ">{item?.price}</h1>
+                                <h2 className="card-title">{item?.name}</h2>
+                                <Link to={'/details/' + item._id}>
+                                    <button className="btn btn-primary">See more</button>
+                                </Link>
                             </div>
-                            <p>{item?.description}</p>
-                           
+
                         </div>
+
                     </div>
                 </div>)
             }
             {
-                category?.length===0 && <h2 className="text-2xl text-center ">
+                category?.length === 0 && <h2 className="text-2xl text-center ">
                     ❌ No available category
                 </h2>
             }
--- src/pages/Home/JobByCategory/JobBycategory.jsx
+++ src/pages/Home/JobByCategory/JobBycategory.jsx
@@ -1,32 +1,69 @@
 import { useEffect, useState } from "react";
 import CategoryCard from "./CategoryCard";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import Category from "../../../CategoryDetails/Category";
+
+
 
 const JobBycategory = () => {
 
     const [categorys, setCategory] = useState([]);
+    const [singleCategory, setSingleCategory] = useState([]);
+    const [selectCategory,setSelectCategory]=useState('all')
 
-    useEffect( () =>{
-        fetch('http://localhost:5000/jobcategory')
-        .then(res => res.json())
-        .then(data => setCategory(data))
-    }, [])
+    useEffect(() => {
+        fetch('http://localhost:5000/jobcategories')
+            .then(res => res.json())
+            .then(data => setCategory(data))
+    }, []);
 
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/job-by-category/${selectCategory}`)
+            .then(res => res.json())
+            .then(data => setSingleCategory(data))
+    },[selectCategory])
+   
+    
+    const handleSelectCategory =(item)=>{
+        setSelectCategory(item)
+    };
+
+console.log(selectCategory);
     return (
         <div>
             <div>
                 <h3 className="text-5xl text-center mt-4
                 font-semibold">Job By
-                 <span className="text-blue-400"> Category</span></h3>
+                    <span className="text-blue-400"> Category</span></h3>
             </div>
-            <div className='grid md:grid-cols-2
+           {/*  <div className='grid md:grid-cols-2
        gap-7 lg:ml-6 mt-10 mb-6 '>
                 {
                     categorys.map(category => <CategoryCard
-                    key={category._id}
-                    category={category}
+                        key={category._id}
+                        category={category}
                     ></CategoryCard>)
                 }
-            </div>
+            </div> */}
+
+            <hr />
+
+            <Tabs>
+                <TabList>
+                    <Tab  onClick={()=>handleSelectCategory('all')}>All</Tab>
+                    {
+                        categorys.map(item => <Tab 
+                            onClick={()=>handleSelectCategory(item.job_category)}
+                        key={item._id}>
+                            {item.job_category}
+                        </Tab>)
+                    }
+                </TabList>
+            </Tabs>
+
+            <Category category={singleCategory}/>
         </div>
     );
 };
